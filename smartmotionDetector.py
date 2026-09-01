b53dab1d6fad557ed9b04dd6774c30e1d60cf975
Richard Akinola <105989001+richexcoder@users.noreply.github.com>
2026-09-01 20:47:02 +0100

    Implement background subtractor in smartmotionDetector
    Added background subtractor for motion detection.

--- smartmotionDetector.py
+++ smartmotionDetector.py
@@ -8,3 +8,10 @@ camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+#Creating our background subtractor
+background_subtractor = cv2.createBackgroundSubtractorMOG2(
+    history=500,
+    varThreshold=50,
+    detectShadows=True
+)
+
